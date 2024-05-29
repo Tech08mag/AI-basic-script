565d216038a30036f4b5538399eb0ec904225e6b
Tech08mag <tech08mag@gmail.com>
2024-05-29 23:32:30 +0200

    take command function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pyttsx3
 import datetime
-
+import speech_recognition as sr
 engine = pyttsx3.init()
 
 
@@ -14,4 +14,29 @@ def time():
     Time = datetime.datetime.now().strftime("%I:%M:%S")
     speak(Time)
 
-time()
+
+def wishme():
+    speak("Willkommen zurück Meister")
+    speak("the current time is")
+    time()
+    speak("Jarvis on your service please tell me how to help you?")
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recongizing...")
+        query =  r.re(audio, language='en-in')
+        print(query)
+    except Exception as e:
+        print(e)
+        speak("Say that again please...")
+        return "None"
+
+    return query
+
+takeCommand()
